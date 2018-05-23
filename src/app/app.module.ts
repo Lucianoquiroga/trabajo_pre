c3039fbab970d3ae713f42b95b4f6499c6af76c5
juanbrz92 <juanbrz92@hotmail.com>
2018-05-23 20:45:51 -0300

    Creando componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,21 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { ContactoComponent } from './components/contacto/contacto.component';
+import { CursosComponent } from './components/cursos/cursos.component';
+import { NosotrosComponent } from './components/nosotros/nosotros.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    ContactoComponent,
+    CursosComponent,
+    NosotrosComponent
   ],
   imports: [
     BrowserModule
