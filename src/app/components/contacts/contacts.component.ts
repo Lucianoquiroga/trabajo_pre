c807344f8249b7dc93bcd5f0e239b4411a1ebb3a
Lucianoquiroga <quiroga_180@hotmail.com>
2018-05-28 19:14:49 -0300

    Subiendo mapa google maps y colores

--- src/app/components/contacts/contacts.component.ts
+++ src/app/components/contacts/contacts.component.ts
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactsComponent implements OnInit {
 
+  lat = -32.896404;
+  lng = -68.841636;
+
   constructor() { }
 
   ngOnInit() {
